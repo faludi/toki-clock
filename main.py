d567f018288c25bed3f52bbe36e9b1d629742691
Rob Faludi <rob@faludi.com>
2025-12-23 12:42:04 -0500

    add various fetch routines

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from machine import Pin, reset
 import network
 import ntptime
 import json
+import requests
 import secrets  # separate file that contains your WiFi credentials
 
 version = "1.0.0"
@@ -14,6 +15,10 @@ password = secrets.WIFI_PASSWORD  # your WiFi password stored in secrets.py
 
 LED = Pin("LED", Pin.OUT)      # digital output for status LED
 
+address = "350 5th Avenue, New York, NY"
+latitude = 40.7484773
+longitude = -73.9881643
+settings_file_url = "http://shinyshape.com/tokiclock/toki_clock_settings.json"
 
 def connect_to_wifi():
     wlan = network.WLAN(network.STA_IF)
@@ -49,6 +54,117 @@ def blink_led(times, interval=0.2):
         LED.off()
         time.sleep(interval)
 
+def fetch_weather_data():
+    try:
+        # Make GET request
+        response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=sunrise,sunset&forecast_days=1", timeout=10)
+        # Get response code
+        response_code = response.status_code
+        # Get response content
+        response_content = response.content
+        weather = response.json()
+        # Print results
+        print('Response code: ', response_code)
+        # print('Response content:', response_content)
+        return weather
+    except Exception as e:
+        print('Error fetching weather data:', e)
+        return None
+    
+def open_config():
+    try:
+        with open('config.json', 'r') as f:
+            config_str = f.read()
+            config = json.loads(config_str)
+            # print(config)
+            return config
+    except OSError:
+        print("Creating configuration file.")
+        try:
+            with open("config.json", "w") as f:
+                config = {"address": "350 5th Avenue, New York, NY", "latitude": 40.7484773, "longitude": -73.9881643, "setttings_file_url": "http://shinyshape.com/windlantern/wind_lantern_settings.json"}
+                json_string = json.dumps(config)
+                # print(config)
+                f.write(json_string)
+                return config
+        except Exception as e:
+            print("Error storing light levels:", e)
+
+def save_config():
+    try:
+        with open("config.json", "r") as f:
+            config_str = f.read()
+            config = json.loads(config_str)
+            if (config.get('address') == address and
+                config.get('latitude') == latitude and
+                config.get('longitude') == longitude and
+                config.get('settings_file_url') == settings_file_url):
+                print("Configuration unchanged, not saving.")
+                return
+    except Exception as e:
+        print("Error reading config for comparison:", e)
+    try:
+        with open("config.json", "w") as f:
+            config = {"address": address, "latitude": latitude, "longitude": longitude, "settings_file_url": settings_file_url}
+            json_string = json.dumps(config)
+            # print(config)
+            f.write(json_string)
+            print("Configuration saved.")
+    except Exception as e:
+        print("Error saving config:", e)
+
+def fetch_address(url):
+    try:
+        # Make GET request
+        response = requests.get(url, timeout=10)
+        # Get response code
+        response_code = response.status_code
+        print('Response code: ', response_code)
+        # response_content = response.content
+        # print('Response content:', response_content)
+        config_raw = response.json()
+        # Print results
+        print('Configuration: ', config_raw)
+        return config_raw
+    except Exception as e:
+        print('Error fetching settings:', e)
+        return None
+
+def fetch_location_from_address(address):
+    try:
+        headers = {
+            "User-Agent": "rp2"  # Add a custom user agent
+        }
+        response = requests.get(f"https://nominatim.openstreetmap.org/search?q={address}&format=json&limit=1", headers=headers, timeout=10)
+        # print(response.content)
+        response_code = response.status_code
+        response_content = response.content
+        location_data = response.json()
+        print('Response code: ', response_code)
+        
+        if location_data:
+            lat = location_data[0]['lat']
+            lon = location_data[0]['lon']
+            print(f"Fetched coordinates for address '{address}': Latitude {lat}, Longitude {lon}")
+            return float(lat), float(lon)
+        else:
+            raise ValueError("Location not found")
+    except Exception as e:
+        print('Error fetching location data:', e)
+        return None, None  
+    
+async def update_location():
+    global address, latitude, longitude, set
+    location = fetch_address(settings_file_url)
+    if location is not None:
+        address = location.get('address')
+        print("Using Address:", address)
+        if address:
+            latitude, longitude = fetch_location_from_address(address.replace(" ", "+"))
+            save_config()
+    else:
+        print("Using default coordinates")
+
 
 def main():
     global microseasons,printer
@@ -73,7 +189,6 @@ def main():
             blink_led(2, 0.1)
             if not connection:
                 break # exit if no connection
-            print("UTC time:", end=' ')
             show_time()
             time.sleep(10)  # main loop delay
 
